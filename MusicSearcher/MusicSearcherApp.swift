275dd710645f67af8cc34a91af54be37ccd31b21
kseo <katieeseo@gmail.com>
2024-12-03 13:25:31 -0500

    Initial Commit

---
+++ MusicSearcher/MusicSearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearcherApp.swift
+//  MusicSearcher
+//
+//  Created by Katie Seo on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicSearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
