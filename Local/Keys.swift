df46733d7ab66b32054e4713430b33735b407ecd
kseo <katieeseo@gmail.com>
2024-12-03 13:26:27 -0500

    Entered api key

---
+++ Local/Keys.swift
@@ -0,0 +1,12 @@
+//
+//  Keys.swift
+//  MusicSearcher
+//
+//  Created by Katie Seo on 12/3/24.
+//
+
+import Foundation
+
+public struct Keys {
+    public static let apikey = "ef0e5f4d1ca1b3d16fa01c811a1af7ea"
+}
